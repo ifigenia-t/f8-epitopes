7bc7b388ec1eb090a9e945f63dd7c219792156eb
Ifigenia Tsitsa <itsits01@mail.bbk.ac.uk>
2018-06-26 22:04:11 +0100

    Add generate pdb files from lists

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
-from utils import get_pdb_sequence, get_uniprot_sequence, get_missing_residues
+import json
 
-pdb_id = input("Enter PDB ID: ")
+from utils import (generate_pepscan_seq, get_missing_residues,
+                   get_pdb_sequence, get_uniprot_sequence,
+                   peptide_list_to_pdb_files)
 
-if pdb_id is '':
-    pdb_id = "2r7e"
+# pdb_id = input("Enter PDB ID: ")
+
+# if pdb_id is '':
+#     pdb_id = "2r7e"
+
+# try:
+#     pdb_seq = get_pdb_sequence(pdb_id)
+#     pdb_seq=pdb_seq[1:]
+#     uniprot_seq = get_uniprot_sequence(pdb_id)
+
+#     print('Uniprot Sequence >>> {}\n'.format(uniprot_seq))
+#     print('Pdb Sequence >>> {}\n'.format(pdb_seq))
+
+#     residues = get_missing_residues(uniprot_seq, pdb_seq)
+
+#     print('# Residues >>> {}'.format(len(residues)))
+#     # for res in residue
+
+
+# except Exception as ex:
+#     print(ex)
+
 
 try:
-    pdb_seq = get_pdb_sequence(pdb_id)
-    uniprot_seq = get_uniprot_sequence(pdb_id)
+    with open('local_data/lists.json') as f:
+        data = json.load(f)
 
-    print('Uniprot Sequence >>> {}\n'.format(uniprot_seq))
-    print('Pdb Sequence >>> {}\n'.format(pdb_seq))
+    # linear = generate_pepscan_seq(data['pepscan_linear'], 5, False)
+    # looped = generate_pepscan_seq(data['pepscan_looped'], 5, True)
+    print('Generating PDB Structure Files...')
+    peptide_list_to_pdb_files(data['pepscan_linear'])
 
-    residues = get_missing_residues(uniprot_seq, pdb_seq)
-
-    print('# Residues >>> {}'.format(len(residues)))
-    # for res in residues:
-    #     print(res)
-
-except Exception as ex:
-    print(ex)
+except Exception as e:
+    print(e)
